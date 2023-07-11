bde4093e478cc6c185c4a243520f2c0f6d475fbd
bruno.oliveira <brunotco4@gmail.com>
2023-07-12 00:38:07 +0100

    implemented notifications

--- Profile/delete.js
+++ Profile/delete.js
@@ -1,20 +1,31 @@
-function deleteAccount() {
-    if (confirm("Are you sure you want to delete your account? This cannot be undone.")) {
-      // Send an AJAX request to delete the account
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "delete.php", true);
-      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200) {
-            // Redirect to the login page
-            alert("Account Deleted!");
-            window.location.href = "../Login";
-          } else {
-            window.location.href = "../Profile";
-          }n
-        }
-      };
-      xhr.send();
+function confirmDelete() {
+  Notify.confirm({
+    title: 'Delete account',
+    description: 'Are you sure you want to delete your account? This cannot be undone.',
+    action: {
+      text: 'Confirm',
+      callback: deleteAccount
     }
+  })
+}
+
+function deleteAccount() {
+  // Send an AJAX request to delete the account
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "delete.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Redirect to the login page
+        Notify.success({
+          description: 'Account deleted'
+        })
+        window.location.href = "../Login";
+      } else {
+        window.location.href = "../Profile";
+      }n
+    }
+  };
+  xhr.send();
 }
