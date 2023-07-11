8088a6de886c3157b888ae05319cb21589941a9c
Propa1 <139003043+Propa1@users.noreply.github.com>
2023-07-11 22:53:58 +0100

    comentary box

---
+++ Comentary/index.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../css/comantary.css">
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+<div class="card">
+    <div class="chat-header">Chat<i class="ri-close-line"></i></div>
+    <div class="chat-window">
+        <ul class="message-list">
+            <img src="../img/man.png" alt="">
+            <label class="UserName">name :</label>
+            <div class="Comentario">
+                <label class="Comentario">isto e um comentario isto e mais um comentario</label>
+            </div>
+        </ul>
+    </div>
+    <div class="chat-input">
+        <input type="text" class="message-input" placeholder="Type your message here">
+        <button class="send-button"><i class="ri-send-plane-line"></i></button>
+    </div>
+</div>
